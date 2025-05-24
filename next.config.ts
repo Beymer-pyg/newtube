36320a743e805353869e6a6edd5a8f4a4be2cebf
Beymer-pyg <beymerpyg@gmail.com>
2025-05-24 12:31:27 -0500

    update ytCLone

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ix15g5sk18.ufs.sh",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
